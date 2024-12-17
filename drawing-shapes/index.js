0569535eed583a363cd3a52c9374d33d0c72c1a7
pery mimon <pery.mimon@gmail.com>
2024-12-17 03:32:50 +0200

    create the TOC

--- drawing-shapes/index.js
+++ drawing-shapes/index.js
@@ -18,20 +18,20 @@ import {drawTree} from './items/tree.js'
 
 var basePath = './items/'
 var drawItemGenerators = []
-drawItemGenerators[0] = {draw: drawStar, filePath: `${basePath}star.js`}
-drawItemGenerators[1] = {draw: drawBall, filePath: `${basePath}ball.js`}
-drawItemGenerators[2] = {draw: drawSock, filePath: `${basePath}sock.js`}
-drawItemGenerators[3] = {draw: drawCane, filePath: `${basePath}cane.js`}
-drawItemGenerators[4] = {draw: drawBow, filePath: `${basePath}bow.js`}
-drawItemGenerators[5] = {draw: drawBell, filePath: `${basePath}bell.js`}
-drawItemGenerators[6] = {draw: drawSnowBall, filePath: `${basePath}snow-ball.js`}
-drawItemGenerators[7] = {draw: drawCandle, filePath: `${basePath}candle.js`}
-drawItemGenerators[8] = {draw: drawGlove, filePath: `${basePath}glove.js`}
-drawItemGenerators[9] = {draw: drawCandy, filePath: `${basePath}candy.js`}
-drawItemGenerators[10] = {draw: drawSnowflake, filePath: `${basePath}snowflake.js`}
-drawItemGenerators[11] = {draw: drawSledge, filePath: `${basePath}sledge.js`}
-drawItemGenerators[12] = {draw: drawTree, filePath: `${basePath}tree.js`}
-drawItemGenerators[13] = {draw: drawHat, filePath: `${basePath}Hat.js`}
-drawItemGenerators[14] = {draw: drawCalendar, filePath: `${basePath}calendar.js`}
+drawItemGenerators[0] =  {name:'Star',draw: drawStar, filePath: `${basePath}star.js`}
+drawItemGenerators[1] =  {name:'Ball',draw: drawBall, filePath: `${basePath}ball.js`}
+drawItemGenerators[2] =  {name:'Sock',draw: drawSock, filePath: `${basePath}sock.js`}
+drawItemGenerators[3] =  {name:'Cane',draw: drawCane, filePath: `${basePath}cane.js`}
+drawItemGenerators[4] =  {name:'Bow',draw: drawBow, filePath: `${basePath}bow.js`}
+drawItemGenerators[5] =  {name:'Bell',draw: drawBell, filePath: `${basePath}bell.js`}
+drawItemGenerators[6] =  {name:'SnowBall',draw: drawSnowBall, filePath: `${basePath}snow-ball.js`}
+drawItemGenerators[7] =  {name:'Candle',draw: drawCandle, filePath: `${basePath}candle.js`}
+drawItemGenerators[8] =  {name:'Glove',draw: drawGlove, filePath: `${basePath}glove.js`}
+drawItemGenerators[9] =  {name:'Candy',draw: drawCandy, filePath: `${basePath}candy.js`}
+drawItemGenerators[10] =  {name:'Snowflake',draw: drawSnowflake, filePath: `${basePath}snowflake.js`}
+drawItemGenerators[11] =  {name:'Sledge',draw: drawSledge, filePath: `${basePath}sledge.js`}
+drawItemGenerators[12] =  {name:'Tree',draw: drawTree, filePath: `${basePath}tree.js`}
+drawItemGenerators[13] =  {name:'Hat',draw: drawHat, filePath: `${basePath}Hat.js`}
+drawItemGenerators[14] =  {name:'Calendar',draw: drawCalendar, filePath: `${basePath}calendar.js`}
 
 export default drawItemGenerators;
