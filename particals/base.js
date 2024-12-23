7825141c63d62bfa1eb3cb8d060aa5071aee3f83
pery mimon <pery.mimon@gmail.com>
2024-12-24 01:20:12 +0200

    connected particles

--- particals/base.js
+++ particals/base.js
@@ -3,26 +3,23 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-gradient.addColorStop(0, '#ffffff');
-gradient.addColorStop(0.5, 'magenta');
-gradient.addColorStop(1, 'blue');
-ctx.fillStyle = gradient;
-ctx.strokeStyle = 'white'
-ctx.lineWidth = 1;
+ctx.fillStyle = 'red'
+ctx.lineWidth = 5;
 
 class Particle {
     constructor (effect) {
         this.effect = effect;
-        this.radius = 5 + Math.random() * 10;
+        this.radius = 5 + Math.random() * 50;
         this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
         this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2);
-        this.vx = Math.random() * 1 - .5
-        this.vy = Math.random() * 1 - .5
+        this.vx = Math.random() * 4 - 2
+        this.vy = Math.random() * 4 - 2
+
     }
 
     draw (ctx) {
-        // ctx.fillStyle = `hsl(${this.x * .5},100%, 50%)`
+
+        ctx.fillStyle = `hsl(${this.x * .5},100%, 50%)`
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
         ctx.fill()
@@ -40,7 +37,6 @@ class Particle {
 class Effect {
     constructor (canvas) {
         this.canvas = canvas;
-        this.ctx = canvas.getContext("2d");
         this.width = canvas.width;
         this.height = canvas.height;
         this.particles = [];
@@ -54,30 +50,12 @@ class Effect {
         }
     }
 
-    handleParticles (ctx) {
-        this.connectParticles(ctx)
+    handleParticles () {
+
         this.particles.forEach((particle) => {
             particle.update()
             particle.draw(ctx);
         })
-    }
-    connectParticles (ctx) {
-        var maxDistance = 100
-        for (let a=0; a<this.numberOfParticles; a++) {
-            for(let b = a; b< this.particles.length; b++) {
-                let pa = this.particles[a]
-                let pb = this.particles[b]
-                let dx = pa.x - pb.x
-                let dy = pa.y - pb.y
-                let dis = Math.hypot(dx, dy);
-                if( dis < maxDistance ) {
-                    ctx.beginPath();
-                    ctx.moveTo(pb.x, pb.y);
-                    ctx.lineTo(pa.x, pa.y)
-                    ctx.stroke()
-                }
-            }
-        }
     }
 }
 
@@ -85,7 +63,6 @@ const effect = new Effect(canvas);
 effect.handleParticles(ctx)
 
 function animation () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     effect.handleParticles(ctx)
     requestAnimationFrame(animation)
 }
