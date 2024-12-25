3844da8d2b12fed646e3c7b781c1ffe9853c5c5a
pery mimon <pery.mimon@gmail.com>
2024-12-25 12:42:52 +0200

    add collision file

---
+++ math/collision-detection.js
@@ -0,0 +1,13 @@
+export function rectCollision(rect1, rect2){
+    return rect1.x < rect2.x + rect2.w &&
+    rect1.x + rect1.w > rect2.x &&
+    rect1.y < rect2.y + rect2.h &&
+    rect1.y + rect1.h > rect2.y
+}
+
+export function circleCollision(circle1, circle2){
+    const dx = circle1.x - circle2.x;
+    const dy = circle1.y - circle2.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    return distance < circle1.radius + circle2.radius;
+}
