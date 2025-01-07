9c77613d9d1c87917befaae91366a8afd89545e5
pery mimon <pery.mimon@gmail.com>
2025-01-08 01:37:07 +0200

    fix style active sidebar link when tab link selected. add memory to .sidebar-link with tab. for the last tab selected

--- home-scripts/sidebar-link.js
+++ home-scripts/sidebar-link.js
@@ -1,22 +1,24 @@
 import {injectProjectInfo} from './project-info.js'
 
-export function processLink() {
+export function processLink () {
     var linkSelector = 'a.sidebar-link, .sidebar-link  a '
     var frameSelector = '#content-frame'
     const links = document.querySelectorAll(linkSelector)
     const contentFrame = document.querySelector(frameSelector)
 
     links.forEach(link => {
-        var parentLink = link.closest('.sidebar-link').querySelector(':scope > a')
+        var sidebarLink =link.closest('.sidebar-link')
+        var parentLink = sidebarLink.querySelector(':scope > a')
         const href = link.getAttribute('href')
-        link.id = href
+         var id = href
             .replace(/^https?:\/\//, '')
             .replace(/[\/.]/g, '-')
             .replace(/(-index.html)?-?$/, '')
+        link.id = id
         link.dataset.href = href
-        link.href = '#' + link.id
+        link.href = '#' + id
 
-        if(parentLink && parentLink !== link){
+        if (parentLink && parentLink !== link) {
             link.dataset.mainLink = parentLink.id
         }
 
@@ -27,7 +29,12 @@ export function processLink() {
             const decodedHash = hash.slice(1)
             var link = document.getElementById(decodedHash)
             if (link) {
-                link.setAttribute('aria-selected', 'true')
+                toggleLink(link)
+                var subActiveLink = link
+                    .closest('.sidebar-link')
+                    .querySelector('[role=tablist]')
+                    ?.querySelector('[aria-selected="true"]')
+                link = subActiveLink ?? link
                 contentFrame.src = link.dataset.href
                 injectProjectInfo(link)
             } else {
@@ -36,6 +43,24 @@ export function processLink() {
         }
     }
 
+    function toggleTab (activeTab) {
+        var tablist = activeTab.closest('[role="tablist"]')
+        tablist?.querySelectorAll('[aria-selected]')
+            .forEach(tab => tab.ariaSelected = String(tab === activeTab))
+    }
+
+    function toggleLink (activeLink) {
+        if (activeLink.role === 'tab') {
+            toggleTab(activeLink)
+            var mainLinkId = activeLink.dataset.mainLink
+            var parentLink = document.getElementById(mainLinkId)
+            toggleLink(parentLink)
+        } else {
+            activeLink?.closest('nav')
+                ?.querySelectorAll('a.sidebar-link, .sidebar-link > a')
+                .forEach(link => link.ariaSelected = String(link === activeLink))
+        }
+    }
 
     window.addEventListener('hashchange', () => { loadContent(); });
     window.addEventListener('load', () => { loadContent() });
