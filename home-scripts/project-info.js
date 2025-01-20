fa41379e47da8d35119a46d09e7067d4510dac7d
pery mimon <pery.mimon@gmail.com>
2025-01-20 22:10:15 +0200

    Add the youtube icon to the titles. fix some stuff in the titles

--- home-scripts/project-info.js
+++ home-scripts/project-info.js
@@ -37,22 +37,25 @@ export function injectProjectInfo2 (project) {
     }
 
     $infoPanel.innerHTML = `
-          <h3 class="info-title>⬡
-            <span id="info-title" data-value="${data.title}">${data.title}</span> |
-            <span id="info-year" data-value="${data.year}">${data.year}</span> |
-            <a href="${data.source}" data-value="${data.source}" class="info-detail" target="_blanck"><strong>⎇Github</strong></a>
-            <a href="${data.npm}"  data-value="${data.npm}" class="info-detail" target="_blanck"><strong>⚙ NPM</strong></a>
-             
-            <div class="info-content">
+          <h3 class="info-header">⬡
+            <span id="info-title" data-value="${data.title}">${data.title}</span> 
+            <span id="info-year" data-value="${data.year}">| ${data.year}</span> 
+            <span class="info-detail" data-value="${data.github || data.npm}"> |
+                <a href="${data.github}" data-value="${data.github}" class="info-detail" target="_blanck">⎇Github</a>
+                <span data-value="${data.npm}" class="info-detail">· <a href="${data.npm}" target="_blanck">⚙ NPM</a></span>
+                <span data-value="${data.tutorial}" class="info-detail">· <a href="${data.tutorial}" target="_blanck">▶YouTube</a></span>
+             </span>
+           
+          </h3>
+           <div class="info-content">
                 <span class="info-detail" data-value="${data.stack}">
-                    <strong>Stack▯</strong>
-                    <span class="info-value info-stack">${data.stack}</span>
-                    <span data-value="${data.teacher}">, seed 
-                        <a href="${teacher?.url}">${teacher?.name}</a>
-                    </span>
+                    <span>Stack▯</span>
+                    <strong class="info-value info-stack">${data.stack}</strong>
+                    <div data-value="${data.teacher}">
+                    seed <a href="${teacher?.url}">${teacher?.name}</a>
+                    </div>
                 </span>
             </div>
-          </h3>
           <div class="bottom-bar">
             <span class="info-description info-content info-detail">
                 ${data.description?.trim() || 'No description available'}
