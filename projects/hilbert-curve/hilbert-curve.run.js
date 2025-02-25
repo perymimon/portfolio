b2f5ad512b9aa4984fe3083a66d373ddbb6e5ece
pery mimon <pery.mimon@gmail.com>
2025-02-25 23:38:39 +0200

    hilbert-curve.run add the cool effect

--- projects/hilbert-curve/hilbert-curve.run.js
+++ projects/hilbert-curve/hilbert-curve.run.js
@@ -1,8 +1,7 @@
 import {FrameEngine} from '../_glossary/FrameEngine.js'
-import Point from '../_glossary/primitive/Point.primitive.js'
 import draw from '../_helpers/draw.js'
 import {hilbert} from '../_math/hilbert-curve.js'
-import {map} from '../_math/math.js'
+import {map, random} from '../_math/math.js'
 
 var canvas = document.getElementById('canvas1')
 var ctx = canvas.getContext('2d')
@@ -23,22 +22,50 @@ for (let i = 0; i < total; i++) {
         .mult(cubeLen)
         .translate({x: cubeLen / 2, y: cubeLen / 2})
 }
+const sparks = new Set()
+
+
+function generateSparks () {
+    let pos = random(0, i)
+    let length = random(10, 30)
+    let ttl = random(30, 100)
+    let hue = random(100, 130)
+    sparks.add({pos, length, ttl, tl: ttl, hue})
+}
+
 
 /* end setup */
 var i = 0
+ctx.globalAlpha = 1
+
 function update () {
-    if(i >= path.length) i = 0
-    ctx.beginPath()
-    ctx.moveTo(path[i].x, path[i].y)
-    ctx.lineTo(path[i+1].x, path[i+1].y)
-    let h = map(i, 0, path.length, 0, 360)
-    ctx.strokeStyle = `hsl(${h}deg 80% 50%)`
-    ctx.lineWidth = 1
-    ctx.stroke()
+    if (i >= path.length) i = 0
+
+    for (let j = i; j >= 0; j--) {
+        ctx.beginPath()
+        ctx.moveTo(path[j].x, path[j].y)
+        ctx.lineTo(path[j + 1].x, path[j + 1].y)
+        let h = map(j, 0, path.length, 0, 360)
+        ctx.strokeStyle = `hsl(${h}deg 80% 50% / 1)`
+        ctx.lineWidth = 1
+        ctx.stroke()
+    }
     i++
+    if (Math.random() < i/path.length) generateSparks()
+    for (let s of sparks) {
+        let miniPath = path.slice(s.pos, s.pos + s.length)
+        if (miniPath.length < s.length) miniPath = path.slice(-s.length)
+        draw.curve(ctx, miniPath, {strokeStyle: `hsl(${s.hue} 100%  50% / ${s.tl / s.ttl}`})
+        s.pos++
+        s.tl--
+        if (s.tl == 0) {
+            sparks.delete(s)
+        }
+
+    }
 }
 
-new FrameEngine(10, function ({detail: {frames}}) {
-    // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+new FrameEngine(30, function ({detail: {frames}}) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     update()
 }).start()
