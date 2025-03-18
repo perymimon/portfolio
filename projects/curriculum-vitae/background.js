4c8ee74c41375d78d454ee5eeffeb3e61d7cd1e1
pery mimon <pery.mimon@gmail.com>
2025-03-18 22:32:19 +0200

    paralex effect to cv background

--- projects/curriculum-vitae/background.js
+++ projects/curriculum-vitae/background.js
@@ -2,32 +2,42 @@ import particlesEffect from '../_glossary/effects/particles.effect.js'
 import {FrameEngine} from '../_glossary/FrameEngine.js'
 import {getProperty, setCanvas} from '../_helpers/basic.js'
 import {getLinearGradient} from '../_helpers/cavas.basic.js'
-var canvas = document.getElementById("canvas1")
-var ctx = canvas.getContext("2d")
 
-// setCanvas(canvas, document.documentElement)
-var element = document.getElementById('particles-background')
-setCanvas(canvas, element)
-var area = canvas.width * canvas.height
-const effect = new particlesEffect(canvas, Math.round (area / 7000) )
-effect.onResize = ()=>{
-    var gradient = getLinearGradient(ctx, 0,0, 0, canvas.height,{
-        0:getProperty(ctx, '--bg-stop-1'),
-        1:getProperty(ctx, '--bg-stop-2'),
-    })
-    ctx.fillStyle = gradient
-    ctx.strokeStyle = gradient
+var canvases = Array.from(document.querySelectorAll('#particles-background canvas'))
+
+var ctxes = canvases.map(canvas => canvas.getContext("2d"))
+
+var effects = ctxes.map(ctx => {
+    let {canvas} = ctx
+    let element = document.getElementById('particles-background')
+    setCanvas(canvas, element)
+    let area = canvas.width * canvas.height
+    const effect = new particlesEffect(canvas, Math.round(area / (7000 * canvases.length) ))
+    effect.onResize = () => {
+
+
+    }
     ctx.lineWidth = 1
-}
-effect.onResize()
-window.addEventListener("resize", e => {
-    let {width, height} =element.getBoundingClientRect()
-    effect.resize(width, height)
+    effect.onResize()
+    window.addEventListener("resize", e => {
+        let {width, height} = element.getBoundingClientRect()
+        effect.resize(width, height)
+    })
+    return effect
 })
 
-
-new FrameEngine(10,()=>{
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    effect.update()
-    effect.draw(ctx)
+new FrameEngine(10, () => {
+    for(let effect of effects) {
+        let {ctx, canvas} = effect
+        let gradient = getLinearGradient(ctx, 0, 0, 0, canvas.height, {
+            0: getProperty(ctx, '--bg-stop-1'),
+            1: getProperty(ctx, '--bg-stop-2'),
+        })
+        ctx.fillStyle = gradient
+        ctx.strokeStyle = gradient
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        effect.update()
+        effect.draw(ctx)
+    }
 }).start()
+
--- projects/curriculum-vitae/falling-sand-bg-effect.js
+++ projects/curriculum-vitae/falling-sand-bg-effect.js
@@ -1,18 +1,22 @@
-import particlesEffect from '../_glossary/effects/particles.effect.js'
-import {FrameEngine} from '../_glossary/FrameEngine.js'
-import {getProperty, setCanvas} from '../_helpers/basic.js'
-import {getLinearGradient} from '../_helpers/cavas.basic.js'
+import {setCanvas} from '../_helpers/basic.js'
+import {renderDOMToCanvas} from '../_helpers/draft-html-draw-canvas.js'
+
 var canvas = document.getElementById("canvas2")
 var ctx = canvas.getContext("2d")
 
 // setCanvas(canvas, document.documentElement)
-var element = document.getElementById('particles-background')
-setCanvas(canvas, element)
+var mainElement = document.querySelector('main')
 
-// var mainElement = document.querySelector('main')
+setCanvas(canvas, mainElement)
+var h1 = document.querySelector('#main-header h1')
+
 // var mainChildren = mainElement.children
 //
-// await import("https://cdn.jsdelivr.net/npm/html2canvas@1.4.1/dist/html2canvas.min.js");
-//
-// const img = await window.html2canvas(element);
-// ctx.drawImage(img, 0, 0);
+// renderDOMToCanvas(mainElement, canvas, 0 , 0 ,{
+//     maxDepth:1,
+//     debug:false,
+//     scaleToFit:false,
+//     preserveImages:false,
+// })
+
+h1.style.color= 'lightblue'
