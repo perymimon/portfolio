86da454ffbb38dfb28301062bf6f6fbb4296c563
pery mimon <pery.mimon@gmail.com>
2025-04-16 19:34:39 +0300

    blog, post page: with giscus comments system

---
+++ src/components/fancy-img-svg.component.js
@@ -0,0 +1,20 @@
+customElements.define("img-svg", class extends HTMLElement {
+    static observedAttributes = ["src"];
+
+    connectedCallback() {
+        if (this.hasAttribute("src")) this.load(this.getAttribute("src"));
+    }
+
+    attributeChangedCallback(name, _, newVal) {
+        if (name === "src") this.load(newVal);
+    }
+
+    async load(url) {
+        try {
+            const svg = await fetch(url).then(r => r.text());
+            this.innerHTML = svg;
+        } catch (err) {
+            this.innerHTML = `<span style="color:var(--color-error);">SVG load error</span>`;
+        }
+    }
+});
